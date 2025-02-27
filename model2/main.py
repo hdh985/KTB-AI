9509a3934a007778245dd1c3c844a7f560dc8944
김경민 <hdh985@naver.com>
2025-02-27 05:33:45 -0800

    test

--- model2/main.py
+++ model2/main.py
@@ -1,9 +1,9 @@
 import uvicorn
+import os
 import Chatmodel_server
 
 app = Chatmodel_server.app
 
 if __name__ == "__main__":
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
-
-# uvicorn main:app --reload
+    port = int(os.environ.get("PORT", 8080))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
