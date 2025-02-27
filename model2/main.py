86b9dfe97dccf2025394dda9f034f6a29fbb1a4b
haebo9 <sjs68as@ajou.ac.kr>
2025-02-27 23:08:43 +0900

    update main

--- model2/main.py
+++ model2/main.py
@@ -1,7 +1,9 @@
-from fastapi import FastAPI
+import uvicorn
+import server
 
-app = FastAPI()
+app = server.app
 
-@app.get("/")
-async def read_root():
-    return {"message": "Hello, Cloud Run!"}
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+
+# uvicorn main:app --reload
